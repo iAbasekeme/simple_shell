cedc0c1c1312c8a16c2b9a9307ab97277369801e
iAbas <abasiekemeetim20@gmail.com>
2023-09-20 18:58:30 +0100

    A function to search executables

--- search_executable.c
+++ search_executable.c
@@ -1,16 +1,25 @@
 #include "shell.h"
 
+/**
+ * search_exec - A function that searches for executable in a file
+ * @comand: command to search for
+ *
+ * Return: Duplicated string of the command
+ */
+
 char *search_exec(char *command)
 {
 	struct stat buf;
 	char *path = _getenv("PATH");
+	char *path_copy;
 	char *directory;
+	char *executable_path;
 
 	if (!path)
 		return (NULL);
-	char *path_copy = strdup(path);
+	path_copy = strdup(path);
 	directory = strtok(path_copy, ":");
-	char *executable_path = malloc(strlen(directory) + strlen(path_copy) + 2);
+	executable_path = malloc(strlen(directory) + strlen(path_copy) + 2);
 	while (directory != NULL)
 	{
 		if (executable_path != NULL)
@@ -18,16 +27,24 @@ char *search_exec(char *command)
 			_strcpy(executable_path, directory);
 			strcat(executable_path, "/");
 			strcat(executable_path, command);
-
 			if (stat(executable_path, &buf) == 0)
 			{
 				free(path);
+				free(path_copy);
 				return (executable_path);
 			}
-			printf("the path:%s\n", executable_path);
 			directory = strtok(NULL, ":");
 		}
 	}
+	if (stat(command, &buf) == 0)
+	{
+		free(path);
+		free(executable_path);
+		return (strdup(command));
+	}
+	free(path_copy);
+	free(executable_path);
+	/**free(command);*/
 	free(path);
 	return (NULL);
 }
