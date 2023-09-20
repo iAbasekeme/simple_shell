4a633db0f06b33a2c0331b50a923e5cbc3cdfc99
iAbas <abasiekemeetim20@gmail.com>
2023-09-20 18:57:42 +0100

    main function

--- main.c
+++ main.c
@@ -1,37 +1,61 @@
 #include "shell.h"
 
+/**
+ * main - A main function
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment
+ *
+ * Return: 0 on success
+ */
 int main(int ac, char *argv[], char *env[])
 {
-	(void)ac;
+	size_t prompdisp, n, i = 0;
+	char **str, *prompt = "#cisfun$ ";
+	char *line = NULL;
+	ssize_t input_read;
+	int j = 0, u = 0;
 
-	char *prompt = "#cisfun$ ";
-	char *line;
-	char **str;
-	int exp_fork;
-	int i = 0;
+	(void)ac;
 
 	while (1)
 	{
-		printf("%s", prompt);
-		fflush(stdout);
-
-		line = get_line();
-
+		if (isatty(0) == 1)
+		{
+			prompdisp = strlen(prompt);
+			write(1, prompt, prompdisp);
+		}
+		input_read = getline(&line, &n, stdin);
+		if (input_read == -1)
+		{
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		while (i < strlen(line) && line[i] != '\0' && line[i] == ' ')
+		{
+			i++;
+			continue;
+		}
 		str = split_string(line);
-		printf("first command: %s\n", str[0]);
+		if (strcmp(str[0], "exit") == 0)
+		{
+			for (; str[u] != NULL; u++)
+			{
+				free(str[u]);
+			}
+			free(str);
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		exc_fork(str, argv, env);
 
-		exp_fork = exc_fork(str, argv, env);
-
-		// printf()
-		// printf("%s", search_ex);
-		// exec = exc_fork(command);
-		// if (exec == 0)
-		// {
-		// 	perror("exec_fork");
-		// }
-
+		while (str[j] != NULL)
+		{
+			free(str[j]);
+			j++;
+		}
 		free(str);
 	}
-
+	free(line);
 	return 0;
 }
