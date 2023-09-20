0610b6cd3b5d79e81275ace42add0b4f852743e7
iAbas <abasiekemeetim20@gmail.com>
2023-09-20 18:57:20 +0100

    A function that gets environment

--- get_environ.c
+++ get_environ.c
@@ -1,30 +1,11 @@
 #include "shell.h"
 
-// char *_getenv(const char *name)
-// {
-// 	// Get a pointer to the start of the environment variable list.
-// 	char **env = environ;
-
-// 	// Iterate over the environment variable list.
-// 	while (*env != NULL)
-// 	{
-// 		// Compare the current environment variable name to the given name.
-// 		if (strcmp(*env, name) == 0)
-// 		{
-// 			// If the names match, return the value of the current environment variable,
-// 			// without the trailing slash.
-// 			char *val = strtok(*env + 1, "/");
-// 			return val;
-// 		}
-
-// 		// Increment the pointer to the next environment variable.
-// 		env++;
-// 	}
-
-// 	// If the environment variable is not found, return NULL.
-// 	return NULL;
-// }
-
+/**
+ * _getenv - A function that gets an environment variable
+ * @name: the name of the environent variable
+ * 
+ * Return: Duplicate of the environment variable, else (NULL)
+*/
 char *_getenv(const char *name)
 {
 	char **env = environ;
@@ -34,7 +15,6 @@ char *_getenv(const char *name)
 	{
 		if (strncmp(val, name, strlen(name)) == 0)
 		{
-			// val = *(env + 1);
 			return strdup(val);
 		}
 	}
