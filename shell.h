c39880648890885c42451e2e6c9da1dfeb0da612
iAbas <abasiekemeetim20@gmail.com>
2023-09-20 18:57:52 +0100

    header file

--- shell.h
+++ shell.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-// #include <sys/wait.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <limits.h>
 #include <fcntl.h>
@@ -14,18 +14,19 @@
 
 extern char **environ;
 
-int main();
+int main(int ac, char *argv[], char *env[]);
 char **split_string(char *str);
-char *get_line(void);
+int *get_line(char **a, size_t *b, ...);
 char *_getenv(const char *name);
 char *search_exec(char *command);
 int exc_fork(char **path, char *argv[], char *envp[]);
 
-// string functions
 unsigned int _strspn(char *s, char *accept);
 int _strlen(char *s);
 char *_strchr(char *s, char c);
-char *_strtok(char *str, const char *delimeter);
-int _strcmp(char *s1, const char *s2);
+int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, const char *src, size_t n);
+char *_strdup(const char *str);
 #endif
