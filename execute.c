68d7c5a21405a73f55c0a545b4003fa083fa87b3
iAbas <abasiekemeetim20@gmail.com>
2023-09-22 08:36:52 +0100

    changes

--- execute.c
+++ execute.c
@@ -1,41 +1,32 @@
 #include "shell.h"
-
 /**
  * _exc_fork - A function that executes a line
  * @path: path to the file
  * @argv: argument vector
  * @envp: environment array
- *
  * Return: status of file
  */
-
 int _exc_fork(char **path, char *argv[], char *envp[])
 {
 	char *exec_func = search_exec(path[0]);
-
 	pid_t pid;
 	int status;
 	int result;
 	int y = 0;
 
 	(void)argv;
-
 	pid = fork();
-
 	if (pid == -1)
 	{
 		perror("fork");
 		return (-1);
 	}
-
 	if (pid == 0)
 	{
 		result = execve(exec_func, path, envp);
-
 		if (result == -1)
 		{
 			perror("execve");
-
 			for (; path[y] != NULL; y++)
 			{
 				free(path[y]);
@@ -46,9 +37,7 @@ int _exc_fork(char **path, char *argv[], char *envp[])
 		}
 	}
 	else
-	{
-		_checkstatus(status);
-	}
+		wait(&status);
 	free(path);
 	free(exec_func);
 	return (status);
