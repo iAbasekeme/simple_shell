a7921dba9dff599da4050fc09a21d3325d69e280
iAbas <abasiekemeetim20@gmail.com>
2023-09-23 01:17:07 +0100

    changed paths

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ int main(int ac, char *argv[], char *env[])
 {
 	size_t prompdisp, n;
 	char **str, *prompt = "#cisfun$ ";
-	char *line = NULL, *line_dup = NULL;
+	char *line = NULL;
 	ssize_t input_read;
-	int j, u = 0, i;
-	char **env = environ;
+	int j, u = 0, i, k, exit_status = EXIT_SUCCESS;
 	(void)ac;
+
 	while (1)
 	{
 		if (isatty(0) == 1)
@@ -27,25 +27,33 @@ int main(int ac, char *argv[], char *env[])
 		if (input_read == -1)
 		{
 			free(line);
-			exit(EXIT_SUCCESS);
+			exit(exit_status);
 		}
 		line[input_read] = '\0';
 		i = 0;
-		while (i < _strlen(line) && line[i] != '\0' && line[i] == ' ')
+		while (i < _strlen(line) && line[i] != '\0' && line[i] == ' ' && line[i] == '\t')
 		{
 			i++;
 			continue;
 		}
 		if ((!line + i))
 			continue;
-		line_dup = _strdup(line + i);
-		str = split_string(line);
+		str = split_string(line + i);
 		if (_strcmp(str[0], "env") == 0)
 		{
-			for (*env = environ; *env != NULL; *env++)
+			for (j = 0; *(environ + j) != NULL; j++)
 			{
-				write(STDOUT_FILENO, *env, _strlen(*env));
+				write(STDOUT_FILENO, *(environ + j), _strlen(*(environ + j)));
+				write(1, "\n", 1);
 			}
+			k = 0;
+			while (str[k] != NULL)
+			{
+				free(str[k]);
+				k++;
+			}
+			free(str);
+			continue;
 		}
 		if (_strcmp(str[0], "exit") == 0)
 		{
@@ -55,10 +63,12 @@ int main(int ac, char *argv[], char *env[])
 			}
 			free(str);
 			free(line);
-			free(line_dup);
 			exit(EXIT_SUCCESS);
 		}
-		_exc_fork(str, argv, env);
+		if (_exc_fork(str, argv, env) == EXIT_FAILURE)
+			exit_status = 127;
+		else
+			exit_status = EXIT_SUCCESS;
 		j = 0;
 
 		while (str[j] != NULL)
@@ -69,6 +79,5 @@ int main(int ac, char *argv[], char *env[])
 		free(str);
 	}
 	free(line);
-	free(line_dup);
 	return (0);
 }
